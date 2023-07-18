69eef2d1286224695ed382e67e19c3322b4cc7b1
Justin Terry <justin.terry@wri.org>
2023-07-18 16:45:52 -0700

    Add surface reflectivity indicator

--- cities_indicators/indicators/built_land_without_tree_cover.py
+++ cities_indicators/indicators/built_land_without_tree_cover.py
@@ -17,8 +17,6 @@ class BuiltLandWithTreeCover:
         built_up_land_count = zonal_stats(zones=city_raster, values=built_up_land, stats_funcs=["count"]).set_index("zone")
         tree_cover_in_built_up_land_count = zonal_stats(zones=city_raster, values=tree_cover_in_built_up_land, stats_funcs=["count"]).set_index("zone")
 
-
         percent_tree_cover_in_built_up_land = tree_cover_in_built_up_land_count / built_up_land_count
-
         return city.boundaries.set_index("index").join(percent_tree_cover_in_built_up_land).rename(columns={"count": "percent_tree_cover_in_built_up_land"})
 
