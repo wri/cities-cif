bcb5baf2cdd96f3b62189407d436dbf636975713
Justin Terry <justin.terry@wri.org>
2023-07-26 10:48:09 -0700

    Revert "Add surface reflectivity indicator"
    This reverts commit 69eef2d1286224695ed382e67e19c3322b4cc7b1.

--- cities_indicators/core.py
+++ cities_indicators/core.py
@@ -3,21 +3,16 @@ from enum import Enum
 
 from cities_indicators.city import SupportedCity, City
 from cities_indicators.indicators.built_land_without_tree_cover import BuiltLandWithTreeCover
-from cities_indicators.indicators.surface_reflectivity import SurfaceReflectivity
 
 
 class Indicator(Enum):
     BUILT_LAND_WITH_TREE_COVER = BuiltLandWithTreeCover
-    SURFACE_REFLECTIVTY = SurfaceReflectivity
 
 
 def get_indicators(cities: List[SupportedCity], indicators: List[Indicator]):
     results = []
-
-    # TODO need to pass admin level and union option
     for city in cities:
         for indicator in indicators:
             results.append(indicator.value().calculate(City(city, admin_level=4)))
 
     return results
-
