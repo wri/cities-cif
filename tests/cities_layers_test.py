255868a3d10f93dfc886fdf1007f4ee72bf5b3d8
Saif <saif.shabou@gmail.com>
2023-08-01 14:10:35 +0200

    Add layers.land_surface_temperature and add cities layers tests

---
+++ tests/cities_layers_test.py
@@ -0,0 +1,22 @@
+from cities_indicators.core import get_indicators, Indicator
+from cities_indicators.city import SupportedCity, City
+from cities_indicators.layers.albedo import Albedo
+from cities_indicators.layers.land_surface_temperature import LandSurfaceTemperature
+
+# city
+geo_name = "IDN-Jakarta"
+admin_level= 4
+city = SupportedCity(geo_name)
+
+# Albedo
+# albedo = Albedo().read(city=City(SupportedCity.IDN_Jakarta, admin_level=4), resolution=0.001)
+# print(albedo)
+
+
+# Lst
+# lst = LandSurfaceTemperature.extract_gee(city ='IDN-Jakarta' , admin_level=4)
+# print(lst)
+
+lst = LandSurfaceTemperature().extract_gee(city = City(SupportedCity.IDN_Jakarta, admin_level = 4))
+print(lst)
+
