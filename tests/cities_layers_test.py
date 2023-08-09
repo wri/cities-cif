e080b72f255fb46cabdf1af24e3dd17aeae30744
Saif <saif.shabou@gmail.com>
2023-08-09 14:22:59 +0200

    layers testing

--- tests/cities_layers_test.py
+++ tests/cities_layers_test.py
@@ -2,20 +2,35 @@ from cities_indicators.core import get_indicators, Indicator
 from cities_indicators.city import SupportedCity, City
 from cities_indicators.layers.albedo import Albedo
 from cities_indicators.layers.land_surface_temperature import LandSurfaceTemperature
-
+from cities_indicators.indicators.built_land_with_high_lst import BuiltUpHighLandSurfaceTemperature
+from cities_indicators.indicators.built_land_with_high_lst_gee import BuiltUpHighLandSurfaceTemperatureGEE
 # city
 geo_name = "IDN-Jakarta"
 admin_level= 4
 city = SupportedCity(geo_name)
 
-# Albedo
+# Read Albedo
 # albedo = Albedo().read(city=City(SupportedCity.IDN_Jakarta, admin_level=4), resolution=0.001)
 # print(albedo)
 
+# Extract albedo
 # albedo = Albedo().extract_gee(city = City(SupportedCity.IDN_Jakarta, admin_level = 4))
 # print(albedo)
 
+# Extract LST
+# lst = LandSurfaceTemperature().extract_gee(city = City(SupportedCity.IDN_Jakarta, admin_level = 4))
+# print(lst)
 
-lst = LandSurfaceTemperature().extract_gee(city = City(SupportedCity.IDN_Jakarta, admin_level = 4))
+
+# Read LST
+# lst = LandSurfaceTemperature().read(city=City(SupportedCity.IDN_Jakarta, admin_level=4), resolution=0.001)
+# print(lst)
+
+# Read LST
+# lst = BuiltUpHighLandSurfaceTemperature().calculate(city=City(SupportedCity.IDN_Jakarta, admin_level=4))
+# print(lst)
+
+# Read LST GEE
+lst = BuiltUpHighLandSurfaceTemperatureGEE().calculate_gee(city=City(SupportedCity.IDN_Jakarta, admin_level=4))
 print(lst)
 
