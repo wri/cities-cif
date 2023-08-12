d1cfc3f98ee8c6d71d81cac737cd4fc2175a0913
Justin Terry <justin.terry@wri.org>
2023-08-11 17:17:29 -0700

    Remove test

--- tests/cities_indicators_test.py
+++ tests/cities_indicators_test.py
@@ -12,7 +12,3 @@ def test_surface_reflectivity():
     indicators = get_indicators(cities=[SupportedCity.IDN_Jakarta], indicators=[Indicator.SURFACE_REFLECTIVTY])
     print(indicators)
 
-
-def test_carto_upload():
-    indicators = get_indicators(cities=[SupportedCity.IDN_Jakarta], indicators=[Indicator.BUILT_LAND_WITH_TREE_COVER])
-    export_carto(indicators)
