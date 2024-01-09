cd973206a043bbd9baf3269a86f03b09836b7e4c
weiqi-tori <zhouwq@umich.edu>
2024-01-09 11:49:16 +0800

    add building height layer

---
+++ city_metrix/layers/average_net_building_height.py
@@ -0,0 +1,40 @@
+from dask.diagnostics import ProgressBar
+import xarray as xr
+import xee
+import ee
+
+from .layer import Layer, get_utm_zone_epsg
+
+
+class AverageNetBuildingHeight(Layer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def get_data(self, bbox):
+        crs = get_utm_zone_epsg(bbox)
+        # ANBH is the average height of the built surfaces, USE THIS
+        # AGBH is the amount of built cubic meters per surface unit in the cell
+        # https://ghsl.jrc.ec.europa.eu/ghs_buH2023.php
+        anbh = (ee.ImageCollection("projects/wri-datalab/GHSL/GHS-BUILT-H-ANBH_R2023A")
+                .filterBounds(ee.Geometry.BBox(*bbox))
+                .select('b1')
+                .mosaic()
+                )
+        
+        ds = xr.open_dataset(
+            ee.ImageCollection(anbh),
+            engine='ee',
+            scale=100,
+            crs=crs,
+            geometry=ee.Geometry.Rectangle(*bbox),
+            chunks={'X': 512, 'Y': 512}
+        )
+
+        with ProgressBar():
+            print("Extracting ANBH layer:")
+            data = ds.b1.compute()
+        
+        # get in rioxarray format
+        data = data.squeeze("time").transpose("Y", "X").rename({'X': 'x', 'Y': 'y'})
+
+        return data
