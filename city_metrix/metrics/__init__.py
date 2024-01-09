95079b2d6863a1c585d6af6fee7f860e1a5f6248
Justin Terry <justin.terry@wri.org>
2024-01-09 15:32:07 -0800

    Add natural areas back

--- city_metrix/metrics/__init__.py
+++ city_metrix/metrics/__init__.py
@@ -3,3 +3,4 @@ from built_land_with_low_surface_reflectivity import built_land_with_low_surface
 from built_land_with_high_land_surface_temperature import built_land_with_high_land_surface_temperature
 from mean_tree_cover import mean_tree_cover
 from urban_open_space import urban_open_space
+from natural_areas import natural_areas
