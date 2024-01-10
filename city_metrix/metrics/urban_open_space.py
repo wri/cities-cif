8e8048f3c54031c7f10cffe6addf6121d5469395
weiqi-tori <zhouwq@umich.edu>
2024-01-10 14:19:51 +0800

    fix urban open space and mean tree cover indicator

--- city_metrix/metrics/urban_open_space.py
+++ city_metrix/metrics/urban_open_space.py
@@ -1,11 +1,11 @@
 from geopandas import GeoDataFrame, GeoSeries
 
-from city_metrix.layers import EsaWorldCover, EsaWorldCoverClass, OSMOpenSpace
+from city_metrix.layers import EsaWorldCover, EsaWorldCoverClass, OpenStreetMap, OpenStreetMapClass
 
 
 def urban_open_space(zones: GeoDataFrame) -> GeoSeries:
     built_up_land = EsaWorldCover(land_cover_class=EsaWorldCoverClass.BUILT_UP)
-    open_space = OSMOpenSpace()
+    open_space = OpenStreetMap(osm_class=OpenStreetMapClass.OPEN_SPACE)
 
     open_space_in_built_land = open_space.mask(built_up_land).groupby(zones).count()
     built_land_counts = built_up_land.groupby(zones).count()
