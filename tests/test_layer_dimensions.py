499355c56e76a36873733a472ddc1f0eae5f9521
Kenn Cartier <kenn.cartier@wri.org>
2024-08-27 14:00:29 -0700

    Removed size count since varies between runs

--- tests/test_layer_dimensions.py
+++ tests/test_layer_dimensions.py
@@ -9,13 +9,10 @@ def test_ndvi_dimensions():
     data = NdviSentinel2(year=2023).get_data(BBOX)
     data_for_map = post_process_layer(data, value_threshold=0.4, convert_to_percentage=True)
 
-    expected_size = 37213
-    actual_size = data_for_map.size
     expected_min = 0
     actual_min = data_for_map.values.min()
     expected_max = 85
     actual_max = data_for_map.values.max()
 
-    assert actual_size == expected_size
     assert actual_min == expected_min
     assert actual_max == expected_max
