3b0ca2fe77995712d01fb5e0bccef9dbf9253682
Ted Wong <ted.wong@wri.org>
2024-08-28 13:48:44 -0400

    added test for impervious surface

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -8,6 +8,7 @@ from city_metrix.layers import (
     EsaWorldCover,
     EsaWorldCoverClass,
     HighLandSurfaceTemperature,
+	ImperviousSurface,
     LandsatCollection2,
     LandSurfaceTemperature,
     NasaDEM,
@@ -77,6 +78,9 @@ def test_high_land_surface_temperature():
     data = HighLandSurfaceTemperature().get_data(BBOX_BRAZIL_LAURO_DE_FREITAS_1)
     assert data.any()
 
+def test_impervious_surface():
+    data = ImperviousSurface().get_data(BBOX_BRAZIL_LAURO_DE_FREITAS_1)
+	assert data.any()
 
 def test_land_surface_temperature():
     mean_lst = LandSurfaceTemperature().get_data(BBOX_BRAZIL_LAURO_DE_FREITAS_1).mean()
