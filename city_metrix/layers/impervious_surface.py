e4bbc23a11a774c4da79371912790397f7e1c81f
Ted Wong <ted.wong@wri.org>
2024-09-04 16:49:14 -0400

    tabs to spaces

--- city_metrix/layers/impervious_surface.py
+++ city_metrix/layers/impervious_surface.py
@@ -7,17 +7,17 @@ from .layer import Layer, get_utm_zone_epsg, get_image_collection
 
 
 class ImperviousSurface(Layer):
-	def __init__(self, **kwargs):
-		super().__init__(**kwargs)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
     def get_data(self, bbox):
         # load impervious_surface
-		dataset = ee.ImageCollection(ee.Image("Tsinghua/FROM-GLC/GAIA/v10").gt(0))  # change_year_index is zero if permeable as of 2018
-		imperv_surf = ee.ImageCollection(dataset
-										 .filterBounds(ee.Geometry.BBox(*bbox))
-										 .select('change_year_index')
-										 .sum()
-										 )
+        dataset = ee.ImageCollection(ee.Image("Tsinghua/FROM-GLC/GAIA/v10").gt(0))  # change_year_index is zero if permeable as of 2018
+        imperv_surf = ee.ImageCollection(dataset
+                                         .filterBounds(ee.Geometry.BBox(*bbox))
+                                         .select('change_year_index')
+                                         .sum()
+                                         )
 
-		data = get_image_collection(imperv_surf, bbox, 100, "imperv surf")
-		return data.change_year_index
+        data = get_image_collection(imperv_surf, bbox, 100, "imperv surf")
+        return data.change_year_index
