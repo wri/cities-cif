ec76dc0c5009b3d8555666b2d2ca30dfe50d3d21
Kenn Cartier <kenn.cartier@wri.org>
2024-09-12 10:57:02 -0700

    switching to setup file

---
+++ .github/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="city_metrix",
+    version="0.1.2",
+    description="Module to calculate various metrics on cities.",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'city_metrix.models.building_classifier': [
+            'building_classifier.pkl',
+        ],
+    },
+    author="Justin Terry",
+    license="MIT",
+    install_requires=[
+        "earthengine-api",
+        "geocube",
+        "odc-stac",
+        "geemap",
+        "pystac-client",
+        "xarray-spatial",
+        "xee",
+        "rioxarray",
+        "utm",
+        "osmnx",
+        "geopandas",
+        "s3fs",
+        "dask>=2023.11.0",
+        "boto3",
+        "exactextract<=0.2.0.dev252",
+        "overturemaps",
+        "scikit-learn>=1.5.1",
+        "scikit-image>=0.24.0"
+    ],
+)
