c2fd09440f886366ae648c552d369680b3223a23
Ted Wong <ted.wong@wri.org>
2024-12-15 11:07:42 -0500

    bug fixes

--- city_metrix/metrics/ghg_emissions.py
+++ city_metrix/metrics/ghg_emissions.py
@@ -1,3 +1,4 @@
+import ee
 import geopandas as gpd
 import numpy as np
 from city_metrix.layers import Layer, CamsGhg
@@ -13,23 +14,23 @@ def ghg_emissions(zones, years=[2023]):
         if not year in SUPPORTED_YEARS:
             raise Exception(f'Unsupported year {year}')
 
-    data_ic = ee.ImageCollection(f'projects/wri-datalab/cams-glob-ant/{species}')
     zones_copy = zones.copy(deep=True)
 
     centroids = [zones.iloc[[i]].centroid for i in range(len(zones))]
     geoms = [ee.Geometry.Point(c.x[i], c.y[i]) for i, c in enumerate(centroids)]
-    
-    for year in years:
-		year_total = gpd.Series([0] * len(zones))
-        year_results = data_ic.filter(ee.Filter.eq('year', year))
-        sectors = list(set(year_results.aggregate_array('sector').getInfo()))
-        sectors.sort()
-        for sector in sectors:
-            results = year_results.filter(ee.Filter.eq('sector', sector)).first().reduceRegions(geoms, ee.Reducer.mean())
-            result_Tg = gpd.GeoDataFrame([i['properties']['mean'] for i in results.getInfo()['features']])
-			result_tonne = result_Tg * 1000000
-            zones_copy[f'{species}_{sector}_{year}'] = result_tonne
-			if sector == 'sum':
-				year_total += result_tonne
-		zones_copy[f'total_{year}'] = year_total
+    for species in SUPPORTED_SPECIES:
+        data_ic = ee.ImageCollection(f'projects/wri-datalab/cams-glob-ant/{species}')
+        for year in years:
+            year_results = data_ic.filter(ee.Filter.eq('year', year))
+            year_total = gpd.GeoDataFrame([0] * len(zones))
+            sectors = year_results.aggregate_array('sector').getInfo()
+            sectors.sort()
+            for sector in sectors:
+                results = year_results.filter(ee.Filter.eq('sector', sector)).first().reduceRegions(geoms, ee.Reducer.mean())
+                result_Tg = gpd.GeoDataFrame([i['properties']['mean'] for i in results.getInfo()['features']])
+                result_tonne = result_Tg * 1000000
+                zones_copy[f'{species}_{sector}_{year}'] = result_tonne
+                if sector == 'sum':
+                    year_total += result_tonne
+            zones_copy[f'total_{year}'] = year_total
     return zones_copy
