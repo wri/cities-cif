aafcfd2769255d0752c46d546c954a9d53fd32e3
Ted Wong <ted.wong@wri.org>
2025-01-11 15:33:54 -0500

    fix mask issue

--- city_metrix/layers/canopy_coverage_by_percentage.py
+++ city_metrix/layers/canopy_coverage_by_percentage.py
@@ -1,4 +1,6 @@
 import ee
+import xarray as xr
+import numpy as np
 
 from .layer import Layer, get_image_collection
 
@@ -7,7 +9,6 @@ class CanopyCoverageByPercentage(Layer):
     Attributes:
         height: int 1..10 min canopy height to count as covered
     """
-
 
     def __init__(self, percentage=33, height=5, reduce_resolution=True, **kwargs):
         super().__init__(**kwargs)
@@ -19,15 +20,17 @@ class CanopyCoverageByPercentage(Layer):
         self.height = height
         self.reduce_resolution = reduce_resolution
 
-    def get_data(self, bbox):
+    def get_data(self, bbox) -> xr.DataArray:
         region = ee.Geometry.BBox(*bbox)
-        coverage_ic = ee.ImageCollection(f"projects/wri-datalab/canopycoverpct/canopycover_{self.percentage}pct_{self.height}m").filterBounds(region)
+        coverage_ic = ee.ImageCollection(f"projects/wri-datalab/canopycoverpct/canopycover_{self.percentage}pct_{self.height}m").filterBounds(region).select('cover_code_sum')
         if self.reduce_resolution:
-            coverage_ic = coverage_ic.map(lambda x: x.reduceResolution(ee.Reducer.mode(), bestEffort=True, maxPixels=128))
+            pop_ic = ee.ImageCollection("WorldPop/GP/100m/pop_age_sex").filterBounds(region)
+            coverage_ic = ee.ImageCollection(coverage_ic.mosaic().setDefaultProjection(coverage_ic.first().projection()).reduceResolution(ee.Reducer.median(), bestEffort=False, maxPixels=10000).clip(region).reproject(crs=pop_ic.first().select('population').projection()))
         data = get_image_collection(
-            coverage_ic,
-            bbox,
-            1,
-            "Canopy coverage"
+            image_collection=coverage_ic,
+            bbox=bbox,
+            scale=100,
+            name="Canopy coverage"
         ).cover_code_sum
+        data = data.where(data==1, 1, np.nan)  # so works correctly as mask
         return data
--- city_metrix/metrics/canopy_covered_population.py
+++ city_metrix/metrics/canopy_covered_population.py
@@ -6,4 +6,6 @@ from city_metrix.layers import CanopyCoverageByPercentage, WorldPop
 def canopy_covered_population(zones: GeoDataFrame, percentage=33, height=5) -> GeoSeries:
     pop_layer = WorldPop()
     coverage_layer = CanopyCoverageByPercentage(percentage=percentage, height=height, reduce_resolution=True)
-    return WorldPop().mask(coverage_layer).groupby(zones).sum() / WorldPop().groupby(zones)
+    access_pop = pop_layer.mask(coverage_layer).groupby(zones).sum()
+    total_pop = pop_layer.groupby(zones).sum()
+    return access_pop / total_pop
