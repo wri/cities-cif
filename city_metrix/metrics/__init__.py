e094141f5645ca54e5b4d8e077effc225968b332
Ted Wong <ted.wong@wri.org>
2025-01-12 18:02:56 -0500

    add demographic-specific metrics

--- city_metrix/metrics/__init__.py
+++ city_metrix/metrics/__init__.py
@@ -5,6 +5,6 @@ from .mean_tree_cover import mean_tree_cover
 from .urban_open_space import urban_open_space
 from .natural_areas import natural_areas
 from .era_5_met_preprocessing import era_5_met_preprocessing
-from .percent_population_access import percent_population_access
+from .percent_population_access import percent_population_access, percent_population_access_elderly, percent_population_access_children, percent_population_access_female, percent_population_access_informal
 from .count_accessible_amenities import count_accessible_amenities
 from .recreational_space_per_capita import recreational_space_per_capita
--- city_metrix/metrics/percent_population_access.py
+++ city_metrix/metrics/percent_population_access.py
@@ -2,12 +2,17 @@ from geopandas import GeoDataFrame, GeoSeries
 import numpy as np
 
 from city_metrix.layers.isoline import Isoline
-from city_metrix.layers.world_pop import WorldPop
+from city_metrix.layers.world_pop import WorldPop, UrbanLandUse
 
 
+def percent_population_access(zones: GeoDataFrame, cityname, amenityname, travelmode, threshold_type, threshold_value, isoline_year=2024, agesex_classes=[], informal_only=False, worldpop_year=2020, aws_profilename=None) -> GeoSeries:
 
-
-def percent_population_access(zones: GeoDataFrame, cityname, amenityname, travelmode, threshold_type, threshold_value, isoline_year=2024, agesex_classes=[], worldpop_year=2020, aws_profilename=None) -> GeoSeries:
+    class InformalTemp(UrbanLandUse):
+        def get_data(self, bbox):
+            data = super().get_data(bbox)
+            result = xr.where(data==3, 1, np.nan).rio.write_crs(data.crs))
+            result = result.assign_attrs(**data.attrs)
+            return result
 
     def get_accessible_population(access_features_layer, popraster_layer, zones):
         if len(access_features_layer.gdf):
@@ -21,7 +26,11 @@ def percent_population_access(zones: GeoDataFrame, cityname, amenityname, travel
     # travelmode is walk, bike, automobile, publictransit (only walk implemented for now)
     # threshold_type is distance or time
     # threshold_value is integer, in minutes or meters
-    population_layer = WorldPop(agesex_classes=agesex_classes, year=worldpop_year)
+    if informal_only:
+        informal_mask = InformalTemp()
+        population_layer = WorldPop(agesex_classes=agesex_classes, year=worldpop_year, masks=[informal_layer,])
+    else:
+        population_layer = WorldPop(agesex_classes=agesex_classes, year=worldpop_year)
     params = {
         'cityname': cityname,
         'amenityname': amenityname,
@@ -61,3 +70,19 @@ def percent_population_access(zones: GeoDataFrame, cityname, amenityname, travel
         result_gdf['access_fraction'] = result_gdf['access_fraction'] * 100
         
     return result_gdf
+
+def percent_population_access_elderly(zones, cityname, amenityname, travelmode, threshold_type, threshold_value, isoline_year=2024, worldpop_year=2020, aws_profilename=None):
+    agesex_classes=['F_60', 'F_65', 'F_70', 'F_75', 'F_80', 'M_60', 'M_65', 'M_70', 'M_75', 'M_80']
+    return percent_population_access(zones=zones, cityname=cityname, amenityname=amenityname, travelmode=travelmode, threshold_type=threshold_type, threshold_value=threshold_value, isoline_year=isoline_year, agesex_classes=agesex_classes, informal_only=False, worldpop_year=worldpop_year, aws_profilename=aws_profilename)
+
+def percent_population_access_children(zones, cityname, amenityname, travelmode, threshold_type, threshold_value, isoline_year=2024, worldpop_year=2020, aws_profilename=None):
+    agesex_classes=['F_0', 'F_1', 'F_5', 'F_10', 'M_0', 'M_1', 'M_5', 'M_10']
+    return percent_population_access(zones=zones, cityname=cityname, amenityname=amenityname, travelmode=travelmode, threshold_type=threshold_type, threshold_value=threshold_value, isoline_year=isoline_year, agesex_classes=agesex_classes, informal_only=False, worldpop_year=worldpop_year, aws_profilename=aws_profilename)
+
+def percent_population_access_female(zones, cityname, amenityname, travelmode, threshold_type, threshold_value, isoline_year=2024, worldpop_year=2020, aws_profilename=None):
+    agesex_classes=['F_0', 'F_1', 'F_5', 'F_10', 'F_15', 'F_20', 'F_25', 'F_30', 'F_35', 'F_40', 'F_45', 'F_50', 'F_55', 'F_60', 'F_65', 'F_70', 'F_75', 'F_80']
+    return percent_population_access(zones=zones, cityname=cityname, amenityname=amenityname, travelmode=travelmode, threshold_type=threshold_type, threshold_value=threshold_value, isoline_year=isoline_year, agesex_classes=agesex_classes, informal_only=False, worldpop_year=worldpop_year, aws_profilename=aws_profilename)
+
+def percent_population_access_informal(zones, cityname, amenityname, travelmode, threshold_type, threshold_value, isoline_year=2024, worldpop_year=2020, aws_profilename=None):
+    agesex_classes=[]
+    return percent_population_access(zones=zones, cityname=cityname, amenityname=amenityname, travelmode=travelmode, threshold_type=threshold_type, threshold_value=threshold_value, isoline_year=isoline_year, agesex_classes=agesex_classes, informal_only=True, worldpop_year=worldpop_year, aws_profilename=aws_profilename)
