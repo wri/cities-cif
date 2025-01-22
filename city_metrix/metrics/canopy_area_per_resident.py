328238776ead20781c26857df0973fdbeadf6de9
Ted Wong <ted.wong@wri.org>
2025-01-22 13:11:46 -0500

    correcting the parameters for the demo-specific metrics

--- city_metrix/metrics/canopy_area_per_resident.py
+++ city_metrix/metrics/canopy_area_per_resident.py
@@ -2,7 +2,7 @@ from geopandas import GeoDataFrame, GeoSeries
 from pandas import Series
 import xarray as xr
 import numpy as np
-from city_metrix.layers import TreeCanopyHeight, WorldPop, UrbanLandUse
+from city_metrix.layers import TreeCanopyHeight, WorldPop, WorldPopClass, UrbanLandUse
 
 
 import xarray as xr
@@ -31,13 +31,13 @@ def canopy_area_per_resident(
     return tree_canopy_height_sum.fillna(0) / world_pop_sum
 
 def canopy_area_per_resident_elderly(zones: GeoDataFrame, height=5) -> GeoSeries:
-    return canopy_area_per_resident(zones, agesex_classes=['F_60', 'F_65', 'F_70', 'F_75', 'F_80', 'M_60', 'M_65', 'M_70', 'M_75', 'M_80'], height=5, informal_only=False)
+    return canopy_area_per_resident(zones, agesex_classes=WorldPopClass.ELDERLY, height=height, informal_only=False)
 
 def canopy_area_per_resident_children(zones: GeoDataFrame, height=5) -> GeoSeries:
-    return canopy_area_per_resident(zones, agesex_classes=['F_0', 'F_1', 'F_5', 'F_10', 'M_0', 'M_1', 'M_5', 'M_10'], height=5, informal_only=False)
+    return canopy_area_per_resident(zones, agesex_classes=WorldPopClass.CHILDREN, height=height, informal_only=False)
 
 def canopy_area_per_resident_female(zones: GeoDataFrame, height=5) -> GeoSeries:
-    return canopy_area_per_resident(zones, agesex_classes=['F_0', 'F_1', 'F_5', 'F_10', 'F_15', 'F_20', 'F_25', 'F_30', 'F_35', 'F_40', 'F_45', 'F_50', 'F_55', 'F_60', 'F_65', 'F_70', 'F_75', 'F_80'], height=5, informal_only=False)
+    return canopy_area_per_resident(zones, agesex_classes=WorldPopClass.CHILDREN, height=height, informal_only=False)
 
 def canopy_area_per_resident_informal(zones: GeoDataFrame, height=5) -> GeoSeries:
-        return canopy_area_per_resident(zones, agesex_classes=[], height=5, informal_only=True)
+        return canopy_area_per_resident(zones, agesex_classes=[], height=height, ulu_class=3)
