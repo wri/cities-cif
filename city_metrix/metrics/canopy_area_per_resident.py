2e59da08e6ebc434258c025c535e57d11e68bd81
Ted Wong <ted.wong@wri.org>
2025-01-22 12:40:10 -0500

    restructured to mimic Tori's method and avoid local classes

--- city_metrix/metrics/canopy_area_per_resident.py
+++ city_metrix/metrics/canopy_area_per_resident.py
@@ -1,22 +1,15 @@
 from geopandas import GeoDataFrame, GeoSeries
+from pandas import Series
 import xarray as xr
 import numpy as np
 from city_metrix.layers import TreeCanopyHeight, WorldPop, UrbanLandUse
 
 
 def canopy_area_per_resident(zones: GeoDataFrame, agesex_classes=[], height=5, informal_only=False) -> GeoSeries:
-    class CanopyTemp(TreeCanopyHeight):
-        def get_data(self, bbox):
-            data = super().get_data(bbox)
-            result = xr.where(data >= height, 1, 0).rio.write_crs(data.crs)
-            result = result.assign_attrs(**data.attrs)
-            return result
-    class InformalTemp(UrbanLandUse):
-        def get_data(self, bbox):
-            data = super().get_data(bbox)
-            result = xr.where(data==3, 1, np.nan).rio.write_crs(data.crs)
-            result = result.assign_attrs(**data.attrs)
-            return result
+
+    canopy_layer = TreeCanopyHeight()
+    canopy_layer_get_data = canopy_layer.get_data
+    canopy_layer.get_data = lambda bbox: xr.where(canopy_layer_get_data(bbox) >= height, 1, 0)
 
     results = []
     for rownum in range(len(zones)):  # Row by row because of memory issues
@@ -25,14 +18,18 @@ def canopy_area_per_resident(zones: GeoDataFrame, agesex_classes=[], height=5, i
         canopy_area = canopy_layer.groupby(onezone).sum().fillna(0)[0]
 
         if informal_only:
-            informal_layer = InformalTemp()
+            # monkey-patch ULU get_data get only informal
+            informal_layer = UrbanLandUse()
+            informal_layer_get_data = informal_layer.get_data
+            informal_layer.get_data = lambda bbox: xr.where(informal_layer_get_data(bbox)==3, 1, np.nan)
+
             pop_layer = WorldPop(agesex_classes=agesex_classes, masks=[informal_layer,])
         else:
             pop_layer = WorldPop(agesex_classes=agesex_classes)
         total_pop = pop_layer.groupby(onezone).sum()[0]
 
         results.append(canopy_area / total_pop)
-    return GeoDataFrame({'canopyarea_per_resident_squaremeter': results, 'geometry': zones['geometry']}).canopyarea_per_resident_squaremeter
+    return Series(results)
 
 def canopy_area_per_resident_elderly(zones: GeoDataFrame, height=5) -> GeoSeries:
     return canopy_area_per_resident(zones, agesex_classes=['F_60', 'F_65', 'F_70', 'F_75', 'F_80', 'M_60', 'M_65', 'M_70', 'M_75', 'M_80'], height=5, informal_only=False)
