98f7753d5a629e986e266a226d77f080dc944cb8
weiqi-tori <zhouwq@umich.edu>
2025-01-22 16:22:14 +0800

    update metrics function

--- city_metrix/metrics/percent_area_impervious.py
+++ city_metrix/metrics/percent_area_impervious.py
@@ -1,15 +1,19 @@
 from geopandas import GeoDataFrame, GeoSeries
-import ee
+
 from city_metrix.layers import Layer, ImperviousSurface
 
 
 def percent_area_impervious(zones: GeoDataFrame) -> GeoSeries:
-    class ImperviousSurfaceFillNA(ImperviousSurface):
-        def get_data(self, bbox):
-            data = super().get_data(bbox)
-            return data.fillna(0)
+    imperv = ImperviousSurface()
 
-    impervious_layer = ImperviousSurface()
-    totalarea_layer = ImperviousSurfaceFillNA()
-    return 100 * impervious_layer.groupby(zones).sum() / totalarea_layer.groupby(zones).count()
-    
+    # monkey‐patch impervious get_data to fill na 
+    imperv_fillna = ImperviousSurface()
+    imperv_fillna_get_data = imperv_fillna.get_data
+    imperv_fillna.get_data = lambda bbox: imperv_fillna_get_data(bbox).fillna(0)
+
+    # count with no NaNs
+    imperv_count = imperv.groupby(zones).count()
+    # count all pixels
+    imperv_fillna_count = imperv_fillna.groupby(zones).count()
+
+    return imperv_count / imperv_fillna_count
