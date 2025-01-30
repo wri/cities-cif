e006a6f58cddeedb2af0e24d73209d302c0b60ef
Ted Wong <ted.wong@wri.org>
2025-01-30 17:28:18 -0500

    stupid typo

--- city_metrix/layers/species_richness.py
+++ city_metrix/layers/species_richness.py
@@ -29,7 +29,7 @@ class SpeciesRichness(Layer):
 
     NUM_CURVEFITS = 200
 
-    def __init__(self, taxon, start_year=2024, end_year=2019, **kwargs):
+    def __init__(self, taxon, start_year=2019, end_year=2024, **kwargs):
         super().__init__(**kwargs)
         self.taxon = taxon.value
         self.start_year = start_year
