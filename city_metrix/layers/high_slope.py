aa4477ce0a149192fa6ca6b368bcfbfd26d9ac38
weiqi-tori <zhouwq@umich.edu>
2025-02-10 16:00:21 +0800

    update structure

--- city_metrix/layers/high_slope.py
+++ city_metrix/layers/high_slope.py
@@ -1,28 +1,35 @@
 import ee
 from xrspatial import slope
 
-from .layer import Layer
+from .layer import Layer, validate_raster_resampling_method
 from .nasa_dem import NasaDEM
+from .layer_geometry import GeoExtent
 
+DEFAULT_SPATIAL_RESOLUTION = 30
+DEFAULT_RESAMPLING_METHOD = 'bilinear'
 
 class HighSlope(Layer):
+    def __init__(self, slope_threshold:int=10, **kwargs):
+        super().__init__(**kwargs)
+        self.slope_threshold = slope_threshold
+
     """
     Attributes:
         spatial_resolution: raster resolution in meters (see https://github.com/stac-extensions/raster)
-        resampling_method: interpolation method used by Google Earth Engine. Default is 'bilinear'. All options are: ('bilinear', 'bicubic', None).
+        resampling_method: interpolation method used by Google Earth Engine. Valid options: ('bilinear', 'bicubic', 'nearest').
     """
+    def get_data(self, bbox: GeoExtent, spatial_resolution: int = DEFAULT_SPATIAL_RESOLUTION,
+                 resampling_method: str = DEFAULT_RESAMPLING_METHOD):
 
-    def __init__(self, spatial_resolution:int=30, resampling_method:str='bilinear', slope_threshold:int=10, **kwargs):
-        super().__init__(**kwargs)
-        self.spatial_resolution = spatial_resolution
-        self.resampling_method = resampling_method
-        self.slope_threshold = slope_threshold
+        spatial_resolution = DEFAULT_SPATIAL_RESOLUTION if spatial_resolution is None else spatial_resolution
+        resampling_method = DEFAULT_RESAMPLING_METHOD if resampling_method is None else resampling_method
+        validate_raster_resampling_method(resampling_method)
 
-    def get_data(self, bbox: tuple[float, float, float, float]):
-        nasa_dem = NasaDEM(spatial_resolution=self.spatial_resolution,
-                           resampling_method=self.resampling_method).get_data(bbox)
+        nasa_dem = NasaDEM().get_data(bbox, spatial_resolution=spatial_resolution,
+                                      resampling_method=resampling_method)
 
         slope_data = slope(nasa_dem)
-        steep_slope = slope_data.where(slope_data >= self.slope_threshold)
+        # filter for steep slope
+        data = slope_data.where(slope_data >= self.slope_threshold)
 
-        return steep_slope
+        return data
