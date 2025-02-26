285242bb452c43551b71d12c50234ad59bd4a125
Ted Wong <ted.wong@wri.org>
2025-02-26 16:00:47 -0500

    change source layer from ESA to ULU, change height thresh from 5 to 3

--- city_metrix/metrics/percent_built_area_without_tree_cover.py
+++ city_metrix/metrics/percent_built_area_without_tree_cover.py
@@ -1,22 +1,25 @@
 from geopandas import GeoDataFrame, GeoSeries
-
-from city_metrix.layers import TreeCanopyHeight, EsaWorldCover, EsaWorldCoverClass
+from city_metrix.layers import TreeCanopyHeight, UrbanLandUse
 
 
 def percent_built_area_without_tree_cover(zones: GeoDataFrame) -> GeoSeries:
     """
     Get percentage of land (assuming zones are based on urban extents)
-    with no tree cover (>5 Global Canopy Height dataset).
+    with no tree cover (>3 Global Canopy Height dataset).
     :param zones: GeoDataFrame with geometries to collect zonal stats on
     :return: Pandas Series of percentages
     """
-    class CanopyAbove5Meters(TreeCanopyHeight):
-        def get_data(self, bbox):
-            data = super().get_data(bbox)
-            return data.where(data >= 5)
+    class CanopyAbove3Meters(TreeCanopyHeight):
+        def get_data(self, bbox, spatial_resolution=10):
+            data = super().get_data(bbox, 10)
+            return data.where(data >= 3)
+    class InformalDevelopment(UrbanLandUse):
+        def get_data(self, bbox, spatial_resolution=10):
+            data = super().get_data(bbox, 10)
+            return data.where(data == 3)
 
-    built_up_layer = EsaWorldCover(land_cover_class=EsaWorldCoverClass.BUILT_UP)
-    tree_cover_layer = CanopyAbove5Meters(spatial_resolution=10)
+    built_up_layer = InformalDevelopment()
+    tree_cover_layer = CanopyAbove3Meters()
 
     built_land = built_up_layer.groupby(zones).count()
     built_land_with_tree_cover = built_up_layer.mask(tree_cover_layer).groupby(zones).count()
