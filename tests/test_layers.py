b79ad269156282b3a579d07c9c65a896e87ecc23
weiqi-tori <zhouwq@umich.edu>
2025-03-05 11:36:03 +0800

    check area of the protected areas are the same, not the coordinates

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -3,13 +3,13 @@ import numpy as np
 from city_metrix.layers import *
 from city_metrix.layers.layer_tools import get_projection_name
 from tests.conftest import EXECUTE_IGNORED_TESTS
-from tests.resources.bbox_constants import BBOX_BRA_LAURO_DE_FREITAS_1, BBOX_USA_OR_PORTLAND_1
+from tests.resources.bbox_constants import BBOX_BRA_LAURO_DE_FREITAS_1, BBOX_USA_OR_PORTLAND_2
 
 # Tests are implemented for an area where we have LULC
 # COUNTRY_CODE_FOR_BBOX = 'BRA'
 # BBOX = BBOX_BRA_LAURO_DE_FREITAS_1
 COUNTRY_CODE_FOR_BBOX = 'USA'
-BBOX = BBOX_USA_OR_PORTLAND_1
+BBOX = BBOX_USA_OR_PORTLAND_2
 BBOX_AS_UTM = BBOX.as_utm_bbox()
 
 def test_albedo():
@@ -174,10 +174,12 @@ def test_overture_buildings():
 
 def test_protected_areas():
     data = ProtectedAreas().get_data(BBOX)
-    # assert np.size(data) > 0
+    area = round(data.area, 4)
+    assert np.size(data) > 0
     assert get_projection_name(data.crs.srs) == 'utm'
     utm_bbox_data = ProtectedAreas().get_data(BBOX_AS_UTM)
-    assert data.equals(utm_bbox_data)
+    utm_bbox_area = round(utm_bbox_data.area, 4)
+    assert area.equals(utm_bbox_area)
 
 def test_riparian_areas():
     data = RiparianAreas().get_data(BBOX)
