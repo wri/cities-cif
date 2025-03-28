5dad80823138b68799d9fc2d794b31ddd4c3d28c
weiqi-tori <zhouwq@umich.edu>
2025-03-28 11:22:54 +0800

    comment out get city by id test for now

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -38,7 +38,10 @@ def test_alos_dsm():
 def test_aqueduct_flood():
     data = AqueductFlood().get_data(BBOX)
     assert np.size(data) > 0
-
+    assert get_projection_name(data.crs) == 'utm'
+    utm_bbox_data = AqueductFlood().get_data(BBOX_AS_UTM)
+    assert get_rounded_gdf_geometry(data, 1).equals(get_rounded_gdf_geometry(utm_bbox_data, 1))
+    
 def test_average_net_building_height():
     data = AverageNetBuildingHeight().get_data(BBOX)
     assert np.size(data) > 0
