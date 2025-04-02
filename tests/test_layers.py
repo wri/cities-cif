1fa8286d36c8668a5c948f0a192a27e7007f4146
weiqi-tori <zhouwq@umich.edu>
2025-04-02 17:28:38 +0800

    add random seed for test

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -1,5 +1,6 @@
 import pytest
 import numpy as np
+import random
 
 from city_metrix.layers import *
 from city_metrix.layers.layer_tools import get_projection_name
@@ -227,11 +228,13 @@ def test_smart_surface_lulc():
 
 def test_species_richness():
     taxon = GBIFTaxonClass.BIRDS
+    random.seed(42)
     data = SpeciesRichness(taxon=taxon).get_data(BBOX)
     assert np.size(data) > 0
     assert get_projection_name(data.crs.srs) == 'utm'
+    random.seed(42)
     utm_bbox_data = SpeciesRichness(taxon=taxon).get_data(BBOX_AS_UTM)
-    assert get_rounded_gdf_geometry(data, 1).geometry.equals(get_rounded_gdf_geometry(utm_bbox_data, 1).geometry)
+    assert get_rounded_gdf_geometry(data, 1).equals(get_rounded_gdf_geometry(utm_bbox_data, 1))
 
 def test_tree_canopy_height():
     data = TreeCanopyHeight().get_data(BBOX)
