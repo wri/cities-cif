ad01a6f2c9eca20612736e1f590196ef3fcd301a
Ted Wong <tgwong@gmail.com>
2025-06-21 11:53:30 -0400

    Speed up countrycode lookup

--- city_metrix/layers/key_biodiversity_areas.py
+++ city_metrix/layers/key_biodiversity_areas.py
@@ -3,6 +3,8 @@ from city_metrix.metrix_model import Layer, get_image_collection, GeoExtent
 from ..constants import GEOJSON_FILE_EXTENSION
 
 AWS_STEM = 'https://wri-cities-indicators.s3.us-east-1.amazonaws.com'
+COUNTRYBBOXES_PATH = 'devdata/inputdata/country_bboxes.geojson'
+COUNTRYBOUNDS_PATH = 'devdata/inputdata/country_boundaries.geojson'
 S3_KBA_PREFIX = 'devdata/inputdata/KBA'
 
 class KeyBiodiversityAreas(Layer):
@@ -24,16 +26,21 @@ class KeyBiodiversityAreas(Layer):
         if self.country_code_iso3 is not None:
             country_code = self.country_code_iso3
         else:
-            country_code = 'global'
+            country_bboxes = GeoDataFrame.from_file(f'{AWS_STEM}/{COUNTRYBBOXES_PATH}')
+            matches = country_bboxes.loc[country_bboxes.intersects(bbox.as_geographic_bbox().polygon)]
+            if len(matches > 1):
+                country_bounds = GeoDataFrame.from_file(f'{AWS_STEM}/{COUNTRYBOUNDS_PATH}')
+                matches = country_bounds.loc[country_bounds.intersects(bbox.as_geographic_bbox().polygon)]
+            country_code_iso3 = matches.countrycode[matches.index[0]]
 
         utm_crs = bbox.as_utm_bbox().crs
 
-        country_data = GeoDataFrame.from_file(f'{AWS_STEM}/{S3_KBA_PREFIX}/KBA_{country_code}.geojson')
-        city_data = country_data.loc[country_data.intersects(bbox.as_geographic_bbox().polygon)]
+        country_kba_data = GeoDataFrame.from_file(f'{AWS_STEM}/{S3_KBA_PREFIX}/KBA_{country_code}.geojson')
+        city_kba_data = country_kba_data.loc[country_kba_data.intersects(bbox.as_geographic_bbox().polygon)]
 
-        if len(city_data) > 0:
-            dissolved_data = city_data.dissolve()
-            data = GeoDataFrame({'id': [0], 'is_kba': 1, 'geometry': dissolved_data.geometry}).to_crs(utm_crs)
+        if len(city_kba_data) > 0:
+            dissolved_kba_data = city_kba_data.dissolve()
+            data = GeoDataFrame({'id': [0], 'is_kba': 1, 'geometry': dissolved_kba_data.geometry}).to_crs(utm_crs)
         else:  # No KBAs intersect with boundary -- return point to avoid empty gdf
             data = GeoDataFrame({'id': [0], 'is_kba': 1, 'geometry': [bbox.centroid]}).set_crs(bbox.crs).to_crs(utm_crs)
         
--- city_metrix/metrics/percent_key_biodiversity_area_not_built_up.py
+++ city_metrix/metrics/percent_key_biodiversity_area_not_built_up.py
@@ -1,4 +1,4 @@
-from geopandas import GeoSeries
+from geopandas import GeoDataFrame, GeoSeries
 
 from city_metrix.constants import CSV_FILE_EXTENSION
 from city_metrix.metrix_model import Metric, GeoZone
@@ -8,6 +8,10 @@ from city_metrix.layers import (
     EsaWorldCoverClass,
     WorldPop
 )
+
+AWS_STEM = 'https://wri-cities-indicators.s3.us-east-1.amazonaws.com'
+COUNTRYBBOXES_PATH = 'devdata/inputdata/country_bboxes.geojson'
+COUNTRYBOUNDS_PATH = 'devdata/inputdata/country_boundaries.geojson'
 
 
 class PercentKeyBiodiversityAreaNotBuiltUp(Metric):
@@ -26,8 +30,18 @@ class PercentKeyBiodiversityAreaNotBuiltUp(Metric):
         self, geo_zone: GeoZone, spatial_resolution: int = None
     ) -> GeoSeries:
 
+        if self.country_code_iso3 is None:
+            country_bboxes = GeoDataFrame.from_file(f'{AWS_STEM}/{COUNTRYBBOXES_PATH}')
+            matches = country_bboxes.loc[country_bboxes.intersects(geo_zone.polygon)]
+            if len(matches) > 1:
+                country_bounds = GeoDataFrame.from_file(f'{AWS_STEM}/{COUNTRYBOUNDS_PATH}')
+                matches = country_bounds.loc[country_bounds.intersects(geo_zone.polygon)]
+            country_code_iso3 = matches.countrycode[matches.index[0]]
+        else:
+            country_code_iso3 = self.country_code_iso3
+
         worldpop_layer = WorldPop()
-        kba_layer = KeyBiodiversityAreas(self.country_code_iso3)
+        kba_layer = KeyBiodiversityAreas(country_code_iso3)
         builtup_layer = EsaWorldCover(EsaWorldCoverClass.BUILT_UP)
 
         kba_area = worldpop_layer.mask(kba_layer).groupby(geo_zone).count()
