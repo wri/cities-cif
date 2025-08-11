0fd071e377f19fa911a422f4c53f98e41d4bf39c
Ted Wong <ted.wong@wri.org>
2025-08-10 21:09:28 -0400

    made CIF-compliant

--- city_metrix/metrics/percent_impervious_surface_on_urbanized_land.py
+++ city_metrix/metrics/percent_impervious_surface_on_urbanized_land.py
@@ -1,13 +1,11 @@
-from geopandas import GeoSeries
-
+import pandas as pd
+from typing import Union
 from city_metrix.constants import CSV_FILE_EXTENSION
 from city_metrix.metrix_model import GeoZone, Metric
 from city_metrix.layers import UrbanExtents, ImperviousSurface, WorldPop
 
 
 DEFAULT_SPATIAL_RESOLUTION = 100
-
-# TODO: layer generation and zonal stats use different spatial resolutions
 
 class PercentImperviousSurfaceOnUrbanizedLand(Metric):
     OUTPUT_FILE_FORMAT = CSV_FILE_EXTENSION
@@ -19,11 +17,15 @@ class PercentImperviousSurfaceOnUrbanizedLand(Metric):
 
     def get_metric(self,
                  geo_zone: GeoZone,
-                 spatial_resolution=DEFAULT_SPATIAL_RESOLUTION) -> GeoSeries:
-        spatial_resolution = DEFAULT_SPATIAL_RESOLUTION if spatial_resolution is None else spatial_resolution
+                 spatial_resolution=DEFAULT_SPATIAL_RESOLUTION) -> Union[pd.DataFrame | pd.Series]:
 
         impervious_layer = ImperviousSurface()
         urban_layer = UrbanExtents()
         area_layer = WorldPop()
-
-        return 100 * area_layer.mask(urban_layer).mask(impervious_layer).groupby(geo_zone).count() / area_layer.mask(urban_layer).groupby(geo_zone).count()
+        fraction = area_layer.mask(urban_layer).mask(impervious_layer).groupby(geo_zone).count() / area_layer.mask(urban_layer).groupby(geo_zone).count()
+        if isinstance(fraction, pd.DataFrame):
+            result = fraction.copy()
+            result['value'] = fraction['value'] * 100
+        else:
+            result = result * 100
+        return result
