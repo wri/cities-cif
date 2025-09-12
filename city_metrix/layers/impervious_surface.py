e16e591c783d7b400cfa6a6e6fefea36993f3e39
Ted Wong <tgwong@gmail.com>
2025-09-12 15:41:58 -0400

    bug fixes for parameter-handling

--- city_metrix/layers/impervious_surface.py
+++ city_metrix/layers/impervious_surface.py
@@ -1,5 +1,6 @@
 import ee
-
+import xarray as xr
+import numpy as np
 from city_metrix.metrix_model import Layer, get_image_collection, GeoExtent
 from ..constants import GTIFF_FILE_EXTENSION
 
@@ -10,7 +11,8 @@ class ImperviousSurface(Layer):
     MAJOR_NAMING_ATTS = None
     MINOR_NAMING_ATTS = None
 
-    def __init__(self, **kwargs):
+    def __init__(self, year=2018, **kwargs):
+        self.year = year
         super().__init__(**kwargs)
 
     def get_data(self, bbox: GeoExtent, spatial_resolution:int=DEFAULT_SPATIAL_RESOLUTION,
@@ -36,5 +38,10 @@ class ImperviousSurface(Layer):
             spatial_resolution,
             "imperv surf"
         ).change_year_index
+
+        # 34 is 1985 impervious
+        # 0 is still pervious as of 2018
+        year_index = 1 + 2018 - self.year
+        data = xr.where(data >= year_index, 1, np.nan).assign_attrs(data.attrs).rio.write_crs(data.crs)
 
         return data
