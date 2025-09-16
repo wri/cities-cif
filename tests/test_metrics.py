0c07ff755951429b9c391c699331d4087fbd4c5b
weiqi-tori <zhouwq@umich.edu>
2025-09-16 16:20:31 +0800

    clean class name

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -124,7 +124,7 @@ def test_era_5_met_preprocess_upenn():
     assert len(indicator) == 24
     assert_metric_stats(indicator[['DHI']], 2, 0.00, 312.15, 24, 0)
 
-def test_ghg_emissions():
+def test_ghg_emissions__tonnes():
     indicator = GhgEmissions__Tonnes().get_metric(IDN_JAKARTA_TILED_ZONES)
     expected_zone_size = len(IDN_JAKARTA_TILED_ZONES.zones)
     actual_indicator_size = len(indicator)
