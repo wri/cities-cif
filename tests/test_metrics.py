d78207e0015dcc071cf4103d204107c129d683a2
Ted Wong <tgwong@gmail.com>
2025-09-24 16:14:48 -0400

    update indicator format in test

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -125,19 +125,19 @@ def test_era_5_met_preprocess_upenn():
 
 def test_future_annual_maxtemp__degreescelsius():
     indicator = FutureAnnualMaxtemp__DegreesCelsius(model_rank=1).get_metric(IDN_JAKARTA_TILED_ZONES)
-    assert indicator[0] == 34.9  # Returns just one number
+    assert indicator.value[0] == 34.9  # Returns just one number
 
 def test_future_days_above_35__days():
     indicator = FutureDaysAbove35__Days(model_rank=1).get_metric(IDN_JAKARTA_TILED_ZONES)
-    assert indicator[0] == 1.7   # Returns just one number
+    assert indicator.value[0] == 1.7   # Returns just one number
 
 def test_future_extreme_precipitation__days():
     indicator = FutureExtremePrecipitationDays__Days(model_rank=1).get_metric(IDN_JAKARTA_TILED_ZONES)
-    assert indicator[0] == 1   # Returns just one number
+    assert indicator.value[0] == 1   # Returns just one number
 
 def test_future_heatwave_frequency__heatwaves():
     indicator = FutureHeatwaveFrequency__Heatwaves(model_rank=1).get_metric(IDN_JAKARTA_TILED_ZONES)
-    assert indicator[0] == 22.2   # Returns just one number
+    assert indicator.value[0] == 22.2   # Returns just one number
 
 def test_ghg_emissions__tonnes():
     indicator = GhgEmissions__Tonnes().get_metric(IDN_JAKARTA_TILED_ZONES)
