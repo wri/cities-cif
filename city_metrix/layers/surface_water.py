aea4a704c795bb325f52d222d754adc21acfba6d
Ted Wong <tgwong@gmail.com>
2025-10-15 16:51:41 -0400

    add tile size param to surface water layer

--- city_metrix/layers/surface_water.py
+++ city_metrix/layers/surface_water.py
@@ -20,6 +20,7 @@ class SurfaceWater(Layer):
     OUTPUT_FILE_FORMAT = GTIFF_FILE_EXTENSION
     MAJOR_NAMING_ATTS = None
     MINOR_NAMING_ATTS = None
+    PROCESSING_TILE_SIDE_M = 5000
 
     """
     Attributes:
