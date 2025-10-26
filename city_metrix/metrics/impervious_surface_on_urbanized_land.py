19950f357654868d4b4394f7a025386a26cedbb9
Ted Wong <tgwong@gmail.com>
2025-10-26 10:47:19 -0400

    add permeable-urbanized metric

--- city_metrix/metrics/impervious_surface_on_urbanized_land.py
+++ city_metrix/metrics/impervious_surface_on_urbanized_land.py
@@ -39,3 +39,37 @@ class ImperviousSurfaceOnUrbanizedLand__Percent(Metric):
             result = 100 * (impervious_area / urban_area)
 
         return result
+
+class PermeableSurfaceOnUrbanizedLand__Percent(Metric):
+    OUTPUT_FILE_FORMAT = CSV_FILE_EXTENSION
+    MAJOR_NAMING_ATTS = None
+    MINOR_NAMING_ATTS = None
+
+    def __init__(self, year=2015, **kwargs):
+        if not year in [1990, 2000, 2005, 2010, 2015]:
+            raise Exception('Only supported years are 1990, 2000, 2005, 2010, 2015.')
+        super().__init__(**kwargs)
+        self.year = year
+        self.unit = 'percent'
+
+    def get_metric(self,
+                   geo_zone: GeoZone,
+                   spatial_resolution=None) -> Union[pd.DataFrame | pd.Series]:
+
+        impervious_layer = ImperviousSurface(year=self.year)
+        urban_layer = UrbanExtents(year=self.year)
+        area_layer = WorldPop()
+
+        impervious_area = area_layer.mask(urban_layer).mask(impervious_layer).groupby(geo_zone).count()
+        urban_area = area_layer.mask(urban_layer).groupby(geo_zone).count()
+
+        if not isinstance(impervious_area, (int, float)):
+            impervious_area = impervious_area.fillna(0)
+
+        if isinstance(impervious_area, pd.DataFrame):
+            result = impervious_area.copy()
+            result['value'] = 100 * (1 - (impervious_area['value'] / urban_area['value']))
+        else:
+            result = 100 * (1 - (impervious_area / urban_area))
+
+        return result
