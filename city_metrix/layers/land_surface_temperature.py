60fcab947e94d33a625052724a2fcc4546c0e560
Ted Wong <tgwong@gmail.com>
2026-01-29 19:44:01 -0500

    change LST calc method to use 95th (or other) percentile in hot 90-day window

--- city_metrix/layers/land_surface_temperature.py
+++ city_metrix/layers/land_surface_temperature.py
@@ -1,4 +1,6 @@
 import ee
+from datetime import datetime, timedelta
+import numpy as np
 
 from city_metrix.metrix_model import Layer, get_image_collection, GeoExtent
 from ..constants import GTIFF_FILE_EXTENSION
@@ -7,7 +9,7 @@ DEFAULT_SPATIAL_RESOLUTION = 30
 
 class LandSurfaceTemperature(Layer):
     OUTPUT_FILE_FORMAT = GTIFF_FILE_EXTENSION
-    MAJOR_NAMING_ATTS = None
+    MAJOR_NAMING_ATTS = ["percentile", "num_seasons"]
     MINOR_NAMING_ATTS = None
 
     """
@@ -15,16 +17,36 @@ class LandSurfaceTemperature(Layer):
         start_date: starting date for data retrieval
         end_date: ending date for data retrieval
     """
-    def __init__(self, start_date="2013-01-01", end_date="2023-01-01", **kwargs):
+    def __init__(self, percentile=95, num_seasons=5, **kwargs):
         super().__init__(**kwargs)
-        self.start_date = start_date
-        self.end_date = end_date
+        self.percentile = percentile
+        self.num_seasons = num_seasons
 
     def get_data(self, bbox: GeoExtent, spatial_resolution:int=DEFAULT_SPATIAL_RESOLUTION,
                  resampling_method=None):
         if resampling_method is not None:
             raise Exception('resampling_method can not be specified.')
         spatial_resolution = DEFAULT_SPATIAL_RESOLUTION if spatial_resolution is None else spatial_resolution
+
+        def get_hottest_date(bbox, start_year, end_year):
+            era5_ic = ee.ImageCollection("ECMWF/ERA5/DAILY")
+            centroid = ee.Geometry.Point(*list(bbox.centroid.coords), proj=bbox.crs)
+            tempdata = (era5_ic
+                        .filterDate(f"{start_year}-01-01", f"{end_year+1}-01-01")
+                        .select("maximum_2m_air_temperature")
+                        .getRegion(centroid, era5_ic.geometry().projection().nominalScale(), era5_ic.geometry().projection().crs())
+                        .getInfo()
+            )
+            dates = [f"{i[0][:4]}-{i[0][4:6]}-{i[0][6:]}" for i in tempdata[1:]]
+            temps = np.array([i[4] for i in tempdata[1:]])
+            maxtemp_idx = np.argmax(temps)
+            hottest_date = dates[maxtemp_idx]
+            result = {
+                "year": hottest_date.split("-")[0],
+                "month": hottest_date.split("-")[1],
+                "day": hottest_date.split("-")[2],
+                }
+            return result
 
         def cloud_mask(image):
             qa = image.select('QA_PIXEL')
@@ -36,24 +58,49 @@ class LandSurfaceTemperature(Layer):
             thermal_band = image.select('ST_B10').multiply(0.00341802).add(149.0).subtract(273.15)
             return thermal_band
 
+        
+        # Define window of 90 days centered on hottest date
+        now = datetime.now()
+        hottest_date_dict = get_hottest_date(bbox, now.year-self.num_seasons-1, now.year-1)
+        window_enddate = datetime.strptime(f"2002-{hottest_date_dict['month']}-{hottest_date_dict['day']}", "%Y-%m-%d") + timedelta(days=45)
+        window_startdate = datetime.strptime(f"2002-{hottest_date_dict['month']}-{hottest_date_dict['day']}", "%Y-%m-%d") - timedelta(days=45)
+        current_year_incomplete = datetime.strptime(f"{now.year}-{window_enddate.month}-{window_enddate.day}", "%Y-%m-%d") > now - timedelta(days=20)
+        start_year_is_previous_year = window_startdate.year < window_enddate.year
+        
+        # Iterate through years and collect images for percentile calc
+        l8_st_list = ee.List([])
         l8 = ee.ImageCollection("LANDSAT/LC08/C02/T1_L2")
-
         ee_rectangle = bbox.to_ee_rectangle()
-        l8_st = (l8
-                 .select('ST_B10', 'QA_PIXEL')
-                 .filter(ee.Filter.date(self.start_date, self.end_date))
-                 .filterBounds(ee_rectangle['ee_geometry'])
-                 .map(cloud_mask)
-                 .map(apply_scale_factors)
-                 .reduce(ee.Reducer.mean())
-                 )
+        
+        if current_year_incomplete:
+            start_year, end_year = now.year-self.num_seasons, now.year-1
+        else:
+            start_year, end_year = now.year-self.num_seasons+1, now.year
+        for year in range(start_year, end_year+1):
+            if start_year_is_previous_year:
+                start_date = f"{year-1}-{window_startdate.month}-{window_startdate.day}"
+            else:
+                start_date = f"{year}-{window_startdate.month}-{window_startdate.day}"
+            end_date = f"{year}-{window_enddate.month}-{window_enddate.day}"
+            print(start_date, end_date)
+            l8_st = (l8
+                    .select('ST_B10', 'QA_PIXEL')
+                    .filter(ee.Filter.date(start_date, end_date))
+                    .filterBounds(ee_rectangle['ee_geometry'])
+                    .map(cloud_mask)
+                    .map(apply_scale_factors)
+                    .reduce(ee.Reducer.mean())
+                    )
 
-        l8_st_ic = ee.ImageCollection(l8_st)
+            l8_st_list = l8_st_list.add(l8_st)
+        
+        # Get 95th (or other) percentile of collected images, pixelwise
+        pctl = ee.ImageCollection(l8_st_list).toBands().reduce(ee.Reducer.percentile([self.percentile]))
         data = get_image_collection(
-            l8_st_ic,
-            ee_rectangle,
-            spatial_resolution,
-            "LST"
-        ).ST_B10_mean
+                pctl,
+                ee_rectangle,
+                spatial_resolution,
+                f"LST_percentile_{self.percentile}"
+            )[f"p{self.percentile}"]
 
         return data
