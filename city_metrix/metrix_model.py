cee6057eeb2fd943e2ae00593a95eaad4ef79bef
Ted Wong <tgwong@gmail.com>
2026-02-18 20:17:13 -0500

    remove debug code

--- city_metrix/metrix_model.py
+++ city_metrix/metrix_model.py
@@ -1273,8 +1273,9 @@ class Layer:
     def _cache_data_by_fishnet_tiles(
         self, bbox, tile_side_m, spatial_resolution, target_uri
     ):
+        from dask.distributed import print as dprint
+        dprint(f"target uri {target_uri}")
         # TODO: Code currently only handles raster data
-
         # Write individual tiles to cache
         delete_s3_file_if_exists(target_uri)
         delete_s3_folder_if_exists(target_uri)
